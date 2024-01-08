4eb7b815c3c93921e7f8b989f84df7def42e2187
DI XU <richard_chui@hotmail.com>
2024-01-08 02:29:00 +0000

    Update StringUtils.js

--- frontend-react/src/utils/StringUtils.js
+++ frontend-react/src/utils/StringUtils.js
@@ -2,6 +2,22 @@ class StringUtils {
     static isEmpty(value) {
         return value === null || value === undefined || value === '';
     }
+    static isPhone(phoneNumber) {
+        const phoneRegex = /^\d{10}$/;
+        return phoneRegex.test(phoneNumber);
+    }
+
+    static isYear(year) {
+        const yearRegex = /^\d{4}$/;
+        const isNum = yearRegex.test(year);
+        if (isNum) {
+            const currentYear = new Date().getFullYear();
+            if (year > 1000 && year <= currentYear) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 module.exports = StringUtils;
